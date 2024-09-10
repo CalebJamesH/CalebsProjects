c6f40c63e708e102446fcd1efafc0f88b232223a
CalebJamesH <calebjames4jesus@gmail.com>
2024-09-10 16:58:13 -0300

    fixed error

--- Learn Node/lesson 3-4/server.js
+++ Learn Node/lesson 3-4/server.js
@@ -11,6 +11,7 @@ const server = http.createServer((req, res) => {
   const greet = _.once(() => {
     console.log('hello')
   });
+  greet()
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
